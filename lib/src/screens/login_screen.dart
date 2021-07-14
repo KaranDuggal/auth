4153e895567abaa85f1b739a7bcdd80b3422ae52
KaranDuggal <duggalkaran400@gmail.com>
2021-07-14 20:36:31 +0530

    not got pack page

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -14,103 +14,106 @@ class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Login Screen")),
-      body: Center(
-        child:SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                // Text('Enter information about PG Owner'),
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: TextFormField(
-                    autofocus: true,
-                    textCapitalization: TextCapitalization.words,
-                    textAlignVertical: TextAlignVertical.center,
-                    onTap: () {},
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.face),
-                      labelText: 'Enter Name',
-                      hintText: "Enter Your Name",
-                      border: OutlineInputBorder()
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(title: Text("Login Screen")),
+        body: Center(
+          child:SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  // Text('Enter information about PG Owner'),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      autofocus: true,
+                      textCapitalization: TextCapitalization.words,
+                      textAlignVertical: TextAlignVertical.center,
+                      onTap: () {},
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.face),
+                        labelText: 'Enter Name',
+                        hintText: "Enter Your Name",
+                        border: OutlineInputBorder()
+                      ),
                     ),
                   ),
-                ),
-                Padding(
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      autofocus: true,
+                      textCapitalization: TextCapitalization.words,
+                      textAlignVertical: TextAlignVertical.center,
+                      onTap: () {},
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.face),
+                        labelText: 'Enter Email',
+                        hintText: "Enter Your Email",
+                        border: OutlineInputBorder()
+                      ),
+                      validator: (value){
+                        if(value!.isEmpty){
+                          return "This field is requied";
+                        }
+                        if(!value.contains('@')){
+                          return "@ requied";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: TextFormField(
+                      autofocus: true,
+                      textCapitalization: TextCapitalization.words,
+                      textAlignVertical: TextAlignVertical.center,
+                      onTap: () {},
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.face),
+                        labelText: 'Password',
+                        hintText: "Enter Your Password",
+                        border: OutlineInputBorder()
+                      ),
+                    ),
+                  ),
+                  Padding(
                   padding: const EdgeInsets.all(15.0),
                   child: TextFormField(
-                    autofocus: true,
-                    textCapitalization: TextCapitalization.words,
-                    textAlignVertical: TextAlignVertical.center,
-                    onTap: () {},
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.face),
-                      labelText: 'Enter Email',
-                      hintText: "Enter Your Email",
-                      border: OutlineInputBorder()
-                    ),
-                    validator: (value){
-                      if(value!.isEmpty){
-                        return "This field is requied";
-                      }
-                      if(!value.contains('@')){
-                        return "@ requied";
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value!.length < 9) {
+                        return 'Phone number must be 9 digits or longer';
                       }
                       return null;
                     },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: TextFormField(
-                    autofocus: true,
-                    textCapitalization: TextCapitalization.words,
-                    textAlignVertical: TextAlignVertical.center,
-                    onTap: () {},
                     decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.face),
-                      labelText: 'Password',
-                      hintText: "Enter Your Password",
-                      border: OutlineInputBorder()
-                    ),
+                        prefixIcon: Icon(Icons.phone),
+                        labelText: 'Phone number',
+                        hintText: "Enter your phone number",
+                        border: OutlineInputBorder()),
                   ),
                 ),
                 Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value!.length < 9) {
-                      return 'Phone number must be 9 digits or longer';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.phone),
-                      labelText: 'Phone number',
-                      hintText: "Enter your phone number",
-                      border: OutlineInputBorder()),
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(SnackBar(content: Text('Processing Data')));
+                      }
+                    },
+                    child: Text('Submit'),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context)
-                          .showSnackBar(SnackBar(content: Text('Processing Data')));
-                    }
-                  },
-                  child: Text('Submit'),
-                ),
-              ),
-              ],
-            )
-          ),
+                ],
+              )
+            ),
+          )
         )
-      )
+      ),
     );
   }
 }
